7d457add0a270f075b86a8f7e9079711bb25eff1
Himanshu Bisht <himanshubisht313@gmail.com>
2023-04-20 16:13:19 +0530

    Extra: merging and sorting two arrays

--- essential-c-and-cpp-concepts/Arrays.cpp
+++ essential-c-and-cpp-concepts/Arrays.cpp
@@ -3,14 +3,31 @@
 using namespace std;
 
 int main() {
-	int n;
+	int array1[5] = {5, 4, 3, 2, 1};
+	int array2[5] = {1,2,3,4,5};
+	int array3[10] = {0};
+	int a = 0;
 
-	cout<<"Enter size: ";
-	cin>>n;
+	for (int i = 0; i < 5; ++i) {
+		array3[a++] = array1[i];
+	};
 
-	int array[n] = {10, 20};
 
-	for (int x: array)
+	for (int i = 0; i < 5; ++i) {
+		array3[a++] = array2[i];
+	}
+
+	for (int i = 0; i < 10; ++i) {
+		for (int j = 0; j < 10; ++j) {
+			if (array3[i] < array3[j]) {
+				int temp = array3[i];
+				array3[i] = array3[j];
+				array3[j] = temp;
+			}
+		}
+	};
+
+	for (int x: array3)
 		cout<<x<<endl;
 
 	return 0;
