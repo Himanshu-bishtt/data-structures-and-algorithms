0ad99d4ab9dddda137e5233df7db4efcb861cba1
Himanshu Bisht <himanshubisht313@gmail.com>
2023-04-23 22:55:12 +0530

    Extra: Modified matrix class into a template

--- essential-c-and-cpp-concepts/MatrixUsingClass.cpp
+++ essential-c-and-cpp-concepts/MatrixUsingClass.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <stdlib.h>
 
+template<class T>
 class Matrix {
   private: 
     int rows;
     int cols;
-    int **matrix;
+    T **matrix;
 
   public:
     Matrix() {
@@ -18,11 +19,11 @@ class Matrix {
       this->cols = cols;
     };
 
-    int ** create_matrix() {
-      this->matrix = (int **)malloc(this->rows * sizeof(int *));
+    T ** create_matrix() {
+      this->matrix = (T **)malloc(this->rows * sizeof(T *));
 
       for (int i = 0; i < this->rows; ++i) {
-        this->matrix[i] = (int *)malloc(this->cols * sizeof(int *));
+        this->matrix[i] = (T *)malloc(this->cols * sizeof(T *));
       };
 
       return this->matrix;
@@ -33,8 +34,10 @@ class Matrix {
 
       for (int i = 0; i < this->rows; ++i) {
         for (int j = 0; j < this->cols; ++j) {
+          T value;
           std::cout<<"Enter element ("<<i<<j<<") : ";
-          std::cin>>this->matrix[i][j];
+          std::cin>>value;
+          this->matrix[i][j] = value;
         };
       };
     };
@@ -63,7 +66,17 @@ class Matrix {
 };
 
 int main() {
-  Matrix m = Matrix(3, 3);
+  int rows, cols;
+
+  std::cout<<"Enter rows and cols: ";
+  std::cin>>rows>>cols;
+
+  if (rows < 0 || cols < 0) {
+    std::cout<<"Rows or Cols cannot be negative"<<std::endl;
+    return -1;
+  }
+
+  Matrix<float> m = Matrix<float>(rows, cols);
 
   m.create_matrix();
   
