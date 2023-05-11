9ac24c7d3ffd69306c539f2b742466fcaa0d2276
Himanshu Bisht <himanshubisht313@gmail.com>
2023-05-11 23:10:41 +0530

    173. More about linked lists

--- linked-lists/linked-list.cpp
+++ linked-lists/linked-list.cpp
@@ -14,6 +14,46 @@ int main() {
   // in C++
   // head = new Node;
 
+  /**
+   * 
+   * Conditions to check whether head is null
+   * 
+   * 1. if (head == NULL)
+   * 
+   * 2. if (head == 0)
+   * 
+   * 3. if (!head)
+   * 
+   * Conditions to check whether head is not null
+   * 
+   * 1. if (head != NULL)
+   * 
+   * 2. if (head != 0)
+   * 
+   * 3. if (head)
+   * 
+  */
+
+
+  /**
+   * 
+   * Conditions to check whether head is last node
+   * 
+   * 1. if (head->next == NULL)
+   * 
+   * 2. if (head->next == 0)
+   * 
+   * 3. if (!head->next)
+   * 
+   * Conditions to check whether head is not last node
+   * 
+   * 1. if (head->next != NULL)
+   * 
+   * 2. if (head->next != 0)
+   * 
+   * 3. if (head->next)
+  */
+
   head->data = 10;
   head->next = 0;
 
