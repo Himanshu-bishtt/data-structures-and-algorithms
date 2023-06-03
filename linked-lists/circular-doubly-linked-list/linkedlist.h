f8d596d4a1e9f904e94c01c085556a500c1292d2
Himanshu Bisht <himanshubisht313@gmail.com>
2023-06-03 22:27:48 +0530

    Extra: Insert in a circular doubly linked list

--- linked-lists/circular-doubly-linked-list/linkedlist.h
+++ linked-lists/circular-doubly-linked-list/linkedlist.h
@@ -40,8 +40,48 @@ int count(struct Node *p) {
 void display(struct Node *p) {
   std::cout<<count(p)<<" Nodes"<<std::endl;
   do {
-    printf("Data: %d, Address: %p, Next: %p, Prev: %p\n", p->data, p, p->next, p->prev);
+    if (p == head)
+      printf("Data: %d, Address: %p, Next: %p, Prev: %p (HEAD)\n", head->data, head, head->next, head->prev);
+    else if (p == tail)
+      printf("Data: %d, Address: %p, Next: %p, Prev: %p (TAIL)\n", tail->data, tail, tail->next, tail->prev);
+    else
+      printf("Data: %d, Address: %p, Next: %p, Prev: %p\n", p->data, p, p->next, p->prev);
     p = p->next;
   } while (p != head);
   std::cout<<"*****************************************************************************"<<std::endl;
+};
+
+void insert(struct Node *p, int index, int data) {
+  int total = count(p);
+
+  if (index < 0 || index > total) {
+    std::cout<<"Invalid index"<<std::endl;
+    return;
+  };
+
+  Node *temp = new Node;
+  temp->data = data;
+  temp->prev = NULL;
+  temp->next = NULL;
+
+  if (index == 0) {
+    // insert at beginning
+    temp->next = p;
+    p->prev = temp;
+    temp->prev = tail;
+    tail->next = temp;
+    head = temp;
+  } else {
+    // insert at other position
+    for (int i = 1; i < index; ++i)
+      p = p->next;
+    temp->next = p->next;
+    temp->prev = p;
+    p->next->prev = temp;
+    p->next = temp;
+  };
+
+  printf("%d insert at pos %d!!\n", data, index);
+
+  return;
 };
--- linked-lists/circular-doubly-linked-list/main.cpp
+++ linked-lists/circular-doubly-linked-list/main.cpp
@@ -2,11 +2,17 @@
 #include "linkedlist.h"
 
 int main() {
-  int arr[] = {34, -1, 54, -44, 612, 54};
+  int arr[] = {34, -1, 54, -4, 61, 54};
   
   int length = sizeof(arr)/sizeof(arr[0]);
 
   create(arr, length);
+
+  display(head);
+
+  insert(head, 0, 100);
+  
+  insert(head, 7, 999);
 
   display(head);
 }
