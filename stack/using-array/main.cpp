dc11821b7ea419c01d710d8f663919376332b3f0
Himanshu Bisht <himanshubisht313@gmail.com>
2023-06-05 22:34:20 +0530

    233. a) created push method

--- stack/using-array/main.cpp
+++ stack/using-array/main.cpp
@@ -6,4 +6,13 @@ int main() {
   std::cout<<"Size: "<<st.SIZE<<std::endl;
   std::cout<<"Top: "<<st.TOP<<std::endl;
   std::cout<<"Array: "<<st.s<<std::endl;
+
+  push(&st, 100);
+  push(&st, 200);
+  push(&st, 300);
+  push(&st, 400);
+  push(&st, 500);
+  push(&st, 600);
+
+  display(st);
 };
--- stack/using-array/stack.h
+++ stack/using-array/stack.h
@@ -14,3 +14,22 @@ void create() {
   st.TOP = -1;
 };
 
+void display(struct Stack st) {
+  std::cout<<"******************************************************"<<std::endl;
+  for (int i = st.TOP; i >= 0; --i) {
+    printf("Top: %d, Index %d, Data: %d\n", st.TOP, i, *(st.s + i));
+  };
+  std::cout<<"******************************************************"<<std::endl;
+};
+
+void push(struct Stack *st, int data) {
+  if (st->TOP == st->SIZE - 1) {
+    std::cout<<"Stack overflow!!"<<std::endl;
+    return;
+  };
+
+  st->TOP++;
+  st->s[st->TOP] = data; 
+  std::cout<<"Inserted "<<data<<std::endl;
+};
+
