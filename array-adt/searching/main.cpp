36cecfecc2c62bb8e939d865609b718d5341a73e
Himanshu Bisht <himanshubisht313@gmail.com>
2023-06-14 19:18:59 +0530

    101. Improving linear search

--- array-adt/searching/main.cpp
+++ array-adt/searching/main.cpp
@@ -6,9 +6,17 @@ int main() {
 
   int length = sizeof(arr)/sizeof(arr[0]);
 
-  int index = binary_search(arr, length, 60);
+  // int index = binary_search(arr, length, 60);
+
+  // printf(index == -1 ? "Element not found\n" : "Element found at index %d\n", index);
+
+  int index = transposition_linear_search(arr, length, 50);
 
   printf(index == -1 ? "Element not found\n" : "Element found at index %d\n", index);
+
+  for (int i = 0; i < length; ++i) printf("%d ", arr[i]);
+
+  printf("\n");
 
   return 0;
 }
--- array-adt/searching/searching.h
+++ array-adt/searching/searching.h
@@ -1,11 +1,30 @@
 #include <iostream>
 #include <math.h>
 
+void swap(int *a, int *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+};
+
 int linear_search(int arr[], int n, int key) {
   for (int i = 0; i < n; ++i)
     if (arr[i] == key) return i;
   return -1;
 };
+
+int transposition_linear_search(int arr[], int n, int key) {
+  for (int i = 0; i < n; ++i) {
+    if (arr[i] == key) {
+      if (i-1 >= 0) {
+        swap(&arr[i], &arr[i-1]);
+        return i-1;
+      };
+      return i;
+    };
+  };
+  return -1;
+}
 
 int binary_search(int arr[], int n, int key) {
   int left = 0, right = n - 1;
