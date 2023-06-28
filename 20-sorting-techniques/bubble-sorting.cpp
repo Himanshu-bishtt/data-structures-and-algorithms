3f9df2d2665a49748d41b628f545c2f4216eab23
Himanshu Bisht <himanshubisht313@gmail.com>
2023-06-28 22:26:41 +0530

    337. Bubble sort

--- 20-sorting-techniques/bubble-sorting.cpp
+++ 20-sorting-techniques/bubble-sorting.cpp
@@ -1,34 +1,40 @@
 #include <iostream>
 
-void swap(int* xp, int* yp) {
-  int temp = *xp;
-  *xp = *yp;
-  *yp = temp;
+void swap(int *x, int *y) {
+  int temp = *x;
+  *x = *y;
+  *y = temp;
 };
 
-int main() {
-  int arr[] = {5, 7, 3, 2, 54, 12, 654};
-
-  int length = sizeof(arr)/sizeof(arr[0]);
-
+void bubble_sort(int arr[], int n) {
   bool swapped = false;
-
-  for (int i = 0; i < length - 1; i++) {
+  for (int i = 0; i < n - 1; ++i) {
     swapped = false;
-    for (int j = 0; j < length - i - 1; j++) {
+    for (int j = 0; j < n - i - 1; ++j) {
       if (arr[j] > arr[j+1]) {
         swap(&arr[j], &arr[j+1]);
         swapped = true;
       };
     };
-    if (swapped == false) break;
+    if (!swapped) break;
   };
+};
 
-  printf("Array...\n");
-
-  for (int i = 0; i < length; ++i) {
-    std::cout<<arr[i]<<" ";
+void print(int arr[], int n) {
+  for (int i = 0; i < n; ++i) {
+    printf("%d ", arr[i]);
   };
-
   printf("\n");
+};
+
+int main() {
+  int arr[] = {8, 5, 7, 3, 2};
+
+  int length = sizeof(arr)/sizeof(arr[0]);
+
+  bubble_sort(arr, length);
+
+  print(arr, length);
+
+  return 0;
 }
