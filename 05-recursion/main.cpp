e2ad97320c3d5d15d91f9c6c041b93b7919eb648
Himanshu Bisht <himanshubisht313@gmail.com>
2023-08-25 13:41:49 +0530

    50. Lets code recursion

---
+++ 05-recursion/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+void fun(int n) {
+  if (n > 0) {
+    printf("%d", n);
+    fun(n-1);
+  };
+};
+
+int main() {
+  int x = 3;
+  fun(3);
+  printf("\n");
+  return 0;
+};
---
+++ 20-sorting-techniques/quick-sort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+void swap(int &x, int &y) {
+  int temp = x;
+  x = y;
+  y = temp;
+};
+
+void quick_sort(int arr[], int n) {
+  int pivot = arr[0];
+  int i = 0; 
+  int j = n - 1;
+
+  while(i < n && j >= 0) {
+    if (arr[i] >)
+  } 
+};
+
+void display(int arr[], int n) {
+  for (int i = 0; i < n; ++i) {
+    printf("%d ", arr[i]);
+  };
+  printf("\n");
+};
+
+int main() {
+  int arr[] = {32, 12, 54, 66, 12, 57};
+
+  int length = sizeof(arr)/sizeof(arr[0]);
+
+  
+}
